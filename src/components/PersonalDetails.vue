d7879a39594aa1a6b45798effc943a32ab055027
jardelkerr <jardelkerr@live.com>
2022-12-05 16:09:06 +0000

    new commit

--- src/components/PersonalDetails.vue
+++ src/components/PersonalDetails.vue
@@ -1,20 +1,27 @@
- <script setup>
+ <script>
 import form from "./onboarding.json";
 import Email from "./Inputs/Email.vue";
 import InputText from "./Inputs/InputText.vue";
 import Dropdown from "./Inputs/Dropdown.vue";
-import Checkbox from "./Inputs/CheckBox.vue";
 import PhoneNum from "./Inputs/Telephone.vue";
-import { placeholder } from "@babel/types";
+import CheckBox from "./Inputs/CheckBox.vue";
+form;
+Email;
+InputText;
+Dropdown;
+CheckBox;
+PhoneNum;
 </script>
 
 <template>
   <h1>{{ form.name }}</h1>
-  <form>
+  <form class="form" @submit.prevent="onSubmit">
     <div v-for="stage in form.stages" :key="stage.name">
       <h2>{{ stage.label }}</h2>
+
       <div id="groups" v-for="group in stage.groups" :key="group.name">
         <h3>{{ group.label }}</h3>
+
         <div
           id="questions"
           v-for="question in group.questions"
@@ -22,6 +29,7 @@ import { placeholder } from "@babel/types";
         >
           <div id="labels">
             <h4>{{ question.label }}</h4>
+
             <Email
               v-if="question.input_type === 'TextEmail'"
               :placeholder="question.placeholder"
@@ -33,6 +41,7 @@ import { placeholder } from "@babel/types";
               required
             />
             <Dropdown
+              id="dropdown"
               v-if="question.input_type === 'DropdownCountry'"
               :placeholder="question.placeholder"
               required
@@ -42,16 +51,13 @@ import { placeholder } from "@babel/types";
               :placeholder="question.placeholder"
               required
             />
-            <input
-              type="checkbox"
-              v-if="question.input_type === 'BoolCheckbox'"
-            />
+            <CheckBox v-if="question.input_type === 'BoolCheckbox'" />
           </div>
         </div>
       </div>
     </div>
     <div>
-      <input type="submit" value="Submit" />
+      <input id="submit" type="submit" value="Submit" />
     </div>
   </form>
 </template>
@@ -67,7 +73,7 @@ form {
   border-radius: 10px;
 }
 #ßlabels {
-  color: #aaa;
+  color: blanchedalmond;
   display: inline-block;
   margin: auto;
   font-size: 0.6em;
@@ -84,6 +90,7 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+  background: blanchedalmond;
 }
 input[type="checkbox"] {
   display: inline-block;
@@ -92,6 +99,7 @@ input[type="checkbox"] {
   position: relative;
   top: 2px;
 }
+
 #questions {
   border-radius: 25px;
   border: 2px solid;
@@ -100,12 +108,19 @@ input[type="checkbox"] {
   max-height: fit-content;
   margin: 25px 0 15px;
 }
-#test {
-  margin-left: auto;
-}
 
-#groups {
-  margin-left: auto;
+#submit {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 12px;
+  background-color: #008cba;
+  transition-duration: 0.4s;
+}
+#submit:hover {
+  background-color: #4caf50; /* Green */
+  margin: auto;
+}
+#dropdown {
+  background: blanchedalmond;
 }
 </style>
 
